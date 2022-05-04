054ebd07a8be783df6cf7783f2ec8928388f39d7
CarlosZeferino <jose.zeferino@gonet.us>
2022-05-04 15:19:12 -0500

    Eureka commit

--- carro-microservice/src/main/java/com/car/microservice/CarroMicroserviceApplication.java
+++ carro-microservice/src/main/java/com/car/microservice/CarroMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.car.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarroMicroserviceApplication {
 
 	public static void main(String[] args) {
