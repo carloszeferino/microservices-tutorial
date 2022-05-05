bd711e967ce1348c82368557026bcad784ec3c61
CarlosZeferino <jose.zeferino@gonet.us>
2022-05-04 19:07:28 -0500

    gateway updated

--- usuario-microservice/src/main/java/com/usuario/microservice/feignclients/ICarFeignClient.java
+++ usuario-microservice/src/main/java/com/usuario/microservice/feignclients/ICarFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.usuario.microservice.models.Car;
 
 
-@FeignClient(name = "car-microservice", url = "http://localhost:8082")
+@FeignClient(name = "car-microservice")
 @RequestMapping("/cars")
 public interface ICarFeignClient {
 
--- usuario-microservice/src/main/java/com/usuario/microservice/feignclients/IMotoFeignClient.java
+++ usuario-microservice/src/main/java/com/usuario/microservice/feignclients/IMotoFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.usuario.microservice.models.Motorcycle;
 
-@FeignClient(name = "moto-microservice", url = "http://localhost:8083")
+@FeignClient(name = "motorcycle-microservice")
 @RequestMapping("/motos")
 public interface IMotoFeignClient {
 	
